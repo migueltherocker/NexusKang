572fc865c31f7f0884089b4a47ce1db6537cb8a3
ctso <ctsoyars@gmail.com>
2010-03-30 23:13:02 +0000

    Fix typo, use Sholes Red as default for sholes devices.
    Change-Id: I4fc8e7a51ff944e7f7285990eaa86ae9703439f1

--- src/com/android/wallpaper/nexus/NexusRS.java
+++ src/com/android/wallpaper/nexus/NexusRS.java
@@ -105,12 +105,6 @@ class NexusRS extends RenderScriptScene implements SharedPreferences.OnSharedPre
         mPrefs.registerOnSharedPreferenceChangeListener(this);
         mPreset = buildColors();
 
-        try {
-            mCurrentPreset = Integer.valueOf(mPrefs.getString("colorScheme", "0"));
-        } catch (NumberFormatException e) {
-            mCurrentPreset = 0;
-        }
-        
         mBackground = mPrefs.getString("background","normal");
         mOptionsARGB.inScaled = false;
         mOptionsARGB.inPreferredConfig = Bitmap.Config.ARGB_8888;
@@ -205,8 +199,6 @@ class NexusRS extends RenderScriptScene implements SharedPreferences.OnSharedPre
         sb.setType(mStateType, "State", RSID_STATE);
         sb.setType(mCommandType, "Command", RSID_COMMAND);
 
-        Log.i("NexusLWP-createScript", "mColorScheme: '" + mCurrentPreset+"'");
-
         sb.setScript(mResources, R.raw.nexus);
 
         Script.Invokable invokable = sb.addInvokable("initPulses");
@@ -269,8 +261,34 @@ class NexusRS extends RenderScriptScene implements SharedPreferences.OnSharedPre
     
     private void createState() {
         mWorldState = new WorldState();
+
+        /* Try to load a user-specified colorscheme */
+
+        try {
+            mCurrentPreset = Integer.valueOf(mPrefs.getString("colorScheme", "0"));
+        } catch (NumberFormatException e) {
+            mCurrentPreset = -1; // We check this again later.
+        }
+
+        try {
+            mWorldState.mode = mResources.getInteger(R.integer.nexus_mode);
+        } catch (Resources.NotFoundException exc) {
+            mWorldState.mode = 0; // standard nexus mode
+        }
+
+        /* Sholes devices may specify nexus_mode=1 which means they want to use
+         * the "sholes red" colorscheme.
+         * 
+         * Other devices should use 'Dust' as the default.
+         */
+        if (mWorldState.mode == 1 && mCurrentPreset == -1) {
+            mCurrentPreset = 6; // Sholes Red
+        } else if (mWorldState.mode == 0 && mCurrentPreset == -1) {
+            mCurrentPreset = 0; // Dust
+        }
+
         makeNewState();
-        
+
         mStateType = Type.createFromClass(mRS, WorldState.class, 1, "WorldState");
         mState = Allocation.createTyped(mRS, mStateType);
         mState.data(mWorldState);
